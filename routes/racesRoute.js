c5c2aa0d9aed96545c71ca3dcb4d3ec010a2d83c
pepijn <pepijn.mommersteeg@gmail.com>
2019-04-05 10:51:39 +0200

    maakt gebruikt van mid voor json en html

--- routes/racesRoute.js
+++ routes/racesRoute.js
@@ -3,7 +3,6 @@ var router = express.Router();
 var racesController = require('../controllers/racesController');
 var racesControllerhtml = require('../controllers/racesController_html');
 var races = require('../models/race');
-var json = false;
 var jwt = require('jsonwebtoken');
 
 /**
@@ -19,12 +18,12 @@ var jwt = require('jsonwebtoken');
  *         schema:
  *           type: array
  */
-router.all('*',jsoncheck);
-//router.get('/', racesControllerhtml.get);
+router.get('/', needjson,verifyToken, racesController.get);
+router.get('/',needshtml, racesControllerhtml.get);
 router.get('/create', racesControllerhtml.getCreate);
 router.get('/:_id', racesControllerhtml.show);
 router.post('/location', racesController.getlocations);
-router.get('/', verifyToken, racesController.get);
+
 router.get('/:_id', racesController.get);
 router.post('/', racesController.post);
 router.delete('/:_id', racesController.delete);
@@ -36,29 +35,29 @@ function verifyToken(req, res, next)
     //get auth header val
     const bearerHeader = req.headers['authorization'];
     if(typeof bearerHeader !== 'undefined'){
-        const bearer = bearerHeader.split(' ');
-        const bearerToken = bearer[1];
-        req.token = bearerToken;
-        var decoded = jwt.verify(token, 'shhhhh');
-        if(decoded != null && decoded)
-        jwt.verify(req.token, 'geheim', function(err, decoded) {
-            if (err) {
-                res.sendStatus(403);
-            }
-            else{
-                next();
-            }
-        });
+        req.token = bearerHeader.replace("Bearer ","");
+        var decoded = jwt.verify(req.token, 'geheim');
+        if(decoded != null && decoded){
+            next();
+        }
+        else{
+            res.sendStatus(403);
+        }
     }else{
         res.sendStatus(403);
     }
 }
-function  jsoncheck(req, res, next){
-
-    if(req.headers["accept"] != undefined && req.headers["accept"] == 'application/json') {
-        json=true;
+function  needshtml(req, res, next) {
+    if (req.headers["accept"] != undefined && req.headers["accept"] == 'application/json') {
+        next('route')
     }
-    next();
+    else{next();}
 }
-
+    function  needjson(req, res, next) {
+        if (req.headers["accept"] != undefined && req.headers["accept"] == 'application/json') {
+            next();
+        } else {
+            next('route');
+        }
+    }
 module.exports = router;
